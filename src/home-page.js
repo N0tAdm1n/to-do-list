e1de512466fc88488ec337160f218cd7f5cf90f8
N0tAdm1n <n0tadm1n29@gmail.com>
2022-09-05 18:01:09 +0530

    Add priority radio button in form

--- src/home-page.js
+++ src/home-page.js
@@ -1,5 +1,6 @@
 import projects from "./projects";
 
+const currentProject = 0;
 export const homepage = () => {
   const content = document.querySelector("#content");
 
@@ -14,18 +15,24 @@ export const homepage = () => {
   const todolist = document.createElement("div");
   todolist.id = "todolist";
 
+  const list = document.createElement("div");
+  list.id = "list";
+  todolist.appendChild(list);
+
   const addButton = document.createElement("div");
   addButton.id = "addButton";
   addButton.textContent = "+";
-  addButton.addEventListener("click", addButtonListener);
-
   todolist.appendChild(addButton);
+
+  addButton.addEventListener("click", addButtonListener);
 
   content.appendChild(todolist);
 
   const footer = document.createElement("div");
   footer.id = "footer";
   content.appendChild(footer);
+
+  // console.log(projects.getProjectList()[currentProject].getProjectName());
 };
 
 //add button event listener
@@ -33,7 +40,7 @@ const addButtonListener = () => {
   if (!document.querySelector("#form")) {
     const addButton = document.querySelector("#addButton");
     addButton.before(createForm());
-    // submitButtonListener();
+    submitButtonListener();
     cancelButtonListener();
   }
 };
@@ -80,7 +87,7 @@ const createForm = () => {
   const dueDate = document.createElement("div");
   dueDate.id = "dueDate";
 
-  const dueDateLabel = document.createElement("Label");
+  const dueDateLabel = document.createElement("label");
   dueDateLabel.textContent = "Date";
   dueDateLabel.htmlFor = "dueDateInput";
   dueDate.appendChild(dueDateLabel);
@@ -92,6 +99,48 @@ const createForm = () => {
 
   form.appendChild(dueDate);
 
+  //
+  //prority
+  const priority = document.createElement("div");
+  priority.id = "priority";
+  //low priority
+  const lowPriorityLabel = document.createElement("label");
+  lowPriorityLabel.textContent = "Low Priority";
+
+  const lowPriorityInput = document.createElement("input");
+  lowPriorityInput.type = "radio";
+  lowPriorityInput.value = "Low";
+  lowPriorityInput.name = "priority";
+  lowPriorityInput.checked = true;
+
+  lowPriorityLabel.appendChild(lowPriorityInput);
+  priority.appendChild(lowPriorityLabel);
+  //medium priority
+  const mediumPriorityLabel = document.createElement("label");
+  mediumPriorityLabel.textContent = "Medium Priority";
+
+  const mediumPriorityInput = document.createElement("input");
+  mediumPriorityInput.type = "radio";
+  mediumPriorityInput.value = "Medium";
+  mediumPriorityInput.name = "priority";
+
+  mediumPriorityLabel.appendChild(mediumPriorityInput);
+  priority.appendChild(mediumPriorityLabel);
+  //high priority
+  const highPriorityLabel = document.createElement("label");
+  highPriorityLabel.textContent = "High Priority";
+
+  const highPriorityInput = document.createElement("input");
+  highPriorityInput.type = "radio";
+  highPriorityInput.value = "High";
+  highPriorityInput.name = "priority";
+
+  highPriorityLabel.appendChild(highPriorityInput);
+  priority.appendChild(highPriorityLabel);
+
+  form.appendChild(priority);
+
+  //
   //add button
   const button = document.createElement("button");
   button.textContent = "Add";
@@ -117,7 +166,27 @@ const removeForm = () => {
   const form = document.querySelector("#form");
   form.innerHTML = "";
   form.remove();
+};
 
-  //testing
-  projects.test();
+//submit Button listener
+const submitButtonListener = () => {
+  const submitButton = document.querySelector("#submit");
+  submitButton.addEventListener("click", submitForm);
+};
+//function to submit the form
+const submitForm = () => {
+  addItemToProject(); //add todo item to current Project
+  // displayItems(); //display todo items
+};
+
+//function to add item to the current project
+const addItemToProject = () => {
+  const title = document.querySelector("#titleInput").value;
+  const description = document.querySelector("#descriptionInput").value;
+  const dueDate = document.querySelector("#dueDateInput").value;
+  const priority = document.querySelector(
+    "input[name='priority']:checked"
+  ).value;
+
+  console.log([title, description, dueDate, priority]);
 };
